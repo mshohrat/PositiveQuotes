7e83b55ad1a94f4d15b7c6cdf1c23af2cda8204c
m.shohrat <meysam.shohrat@snapp.cab>
2020-07-29 17:36:35 +0430

    add modified_at column to database table

--- app/src/main/java/com/ms/quokkaism/db/AppDatabase.kt
+++ app/src/main/java/com/ms/quokkaism/db/AppDatabase.kt
@@ -3,6 +3,8 @@ package com.ms.quokkaism.db
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ms.quokkaism.App
 import com.ms.quokkaism.db.model.LikeAction
 import com.ms.quokkaism.db.model.Quote
@@ -20,7 +22,9 @@ abstract class AppDatabase : RoomDatabase() {
         fun getAppDataBase(): AppDatabase? {
             if (INSTANCE == null){
                 synchronized(AppDatabase::class){
-                    INSTANCE = Room.databaseBuilder(App.appContext, AppDatabase::class.java, "myDB").build()
+                    INSTANCE = Room.databaseBuilder(App.appContext, AppDatabase::class.java, "myDB")
+                        .addMigrations(MIGRATION_1_2)
+                        .build()
                 }
             }
             return INSTANCE
@@ -28,6 +32,13 @@ abstract class AppDatabase : RoomDatabase() {
 
         fun destroyDataBase(){
             INSTANCE = null
+        }
+
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE quotes ADD COLUMN modified_at LONG default NULL")
+            }
+
         }
     }
 }
